69a39238b9cf7893f0782face0a281149871fadc
shantidyah <shanti.dyah@gmail.com>
2018-05-10 17:37:31 +0700

    callback belanja

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ let cheesecakes = {item : 'cheesecakes',harga : 225000, waktu: 1000};
 let chocolatecakes = {item : 'chocolatecakes',harga : 200000, waktu: 1000};
 let rainbowcakes = {item : 'rainbowcakes',harga : 215000, waktu: 1000};
 let donuts = {item : 'donuts',harga : 15000, waktu: 1000};
+let uang = 300000;
 
-beli(100000,cupcakes,function(kembalian){
+beli(uang,cupcakes,function(kembalian){
   beli(kembalian,cheesecakes,function(kembalian){
     beli(kembalian,chocolatecakes,function(kembalian){
       beli(kembalian,rainbowcakes,function(kembalian){
